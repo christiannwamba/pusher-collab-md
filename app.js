d7f5eba72a85e3c8be023c82cee08737cd2da8ee
Christian Nwamba <nwambachristian@gmail.com>
2017-11-10 12:56:09 +0100

    Helpful comments

--- app.js
+++ app.js
@@ -1,46 +1,69 @@
 class App {
   constructor() {
+    // Initialize properties
     this.textSyncInstance = null;
     this.$ = $;
     this.md = markdownit();
+    // Prepare TextSync
     this.setupTextSync();
   }
 
   setupTextSync() {
+    // Create an instance of TextSync
     this.textSyncInstance = new TextSync({
+      // Unique ID to be shared among connected
+      // clients
       instanceLocator: 'v1:us1:d50eab0e-8d45-4b90-bdbe-bb9ad02bf18c'
     });
     this.textSyncEditor = this.textSyncInstance.createEditor({
+      // Serves as documents primary keu
       docId: 'my-first-document',
-      element: '.editor',
-      // richText: false
+      // What element should I mount the editor?
+      // Uses the CSS selector
+      element: '.editor'
     })
+    // The editor needs to be ready before
+    // you can interact with it. It returns
+    // a promise so you can pass a callback to
+    // take care of resolving the promise
     this.textSyncEditor.then(this.handleTextSyncReady.bind(this));
   }
   
   handleTextSyncReady(editor) {
-    console.log(editor);
+    // Update editor header with
+    // different visuals
     this.updateEditorHeader()
+    // Start an interval to parse the markdown file.
+    // The promise resolves with a payload, `editor`.
     this.startParsingInterval(editor);
   }
 
   updateEditorHeader() {
-    $('.ql-formats').css({'display': 'none'});
-    $('<span class="has-text-weight-semibold">Go markdown</span>').prependTo('.ql-toolbar');
+    // Hide the text formatting controls
+    this.$('.ql-formats').css({'display': 'none'});
+    // Add a random title to replace the controls
+    this.$('<span class="has-text-weight-semibold">Go markdown</span>').prependTo('.ql-toolbar');
   }
 
   startParsingInterval(editor) {
+    // An interval of 3 seconds
     setInterval(() => {
+      // Parse markdown to HTML 
+      // every 3 seconds
       this.parseMarkdown(editor);
     }, 3000)
   }
 
   parseMarkdown(editor) {
+    // `editor.get` retrieves the current
+    // text in the editor
     const rawMdText = editor.getText();
+    // `md.render` parses the MD
     const parsedMdContent = this.md.render(rawMdText);
-    console.log(parsedMdContent);
-    $('.preview__content').html(parsedMdContent);
+    // Replace the preview div with the parsed HTML
+    this.$('.preview__content').html(parsedMdContent);
   }
 }
 
+// Start App
 (new App());
